e0c0bdfd9ad5191f12c42ef822453cc58948a700
Rolf van de Krol <info@rolfvandekrol.nl>
2013-10-18 09:48:50 +0200

    Add form fieldset summary js

---
+++ hopform/hopform.js
@@ -0,0 +1,21 @@
+
+(function ($) {
+
+Drupal.behaviors.hopformFormFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset.hopform-form-form-options', context).drupalSetSummary(function (context) {
+      var vals = [];
+
+      $('input:checked', context).parent().each(function () {
+        vals.push(Drupal.checkPlain($.trim($(this).text())));
+      });
+
+      if (!$('.form-item-status input', context).is(':checked')) {
+        vals.unshift(Drupal.t('Not published'));
+      }
+      return vals.join(', ');
+    });
+  }
+};
+
+})(jQuery);
