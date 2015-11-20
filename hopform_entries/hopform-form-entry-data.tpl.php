1b351c14bceb8febcbd0254ab81f8ea72d497947
Rolf van de Krol <info@rolfvandekrol.nl>
2015-11-20 16:45:35 +0100

    Allow uploads

--- hopform_entries/hopform-form-entry-data.tpl.php
+++ hopform_entries/hopform-form-entry-data.tpl.php
@@ -10,5 +10,15 @@
     <?php print $elements['#children']; ?>
   </div>
 <?php else: ?>
-  <p class="hopform-form-entry-data"><?php print !empty($elements['#label']) ? ('<strong>' . check_plain($elements['#label'] . ': ') . '</strong>') : ''; ?><?php print !empty($elements['#value']) ? check_plain($elements['#value']) : ''; ?></p>
+  <p class="hopform-form-entry-data">
+    <?php print !empty($elements['#label']) ? ('<strong>' . check_plain($elements['#label'] . ': ') . '</strong>') : ''; ?>
+
+    <?php if (!empty($elements['#url'])): ?>
+      <a href="<?php print check_plain($elements['#url']); ?>">
+        <?php print !empty($elements['#value']) ? check_plain($elements['#value']) : ''; ?>
+      </a>
+    <?php else: ?>
+      <?php print !empty($elements['#value']) ? check_plain($elements['#value']) : ''; ?>
+    <?php endif; ?>
+  </p>
 <?php endif; ?>
